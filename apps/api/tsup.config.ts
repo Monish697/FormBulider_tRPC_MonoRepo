4564dbaee227f6851bcfdf4145048e03a57dad48
MonishPanjwani <monishpunjwani55@gmail.com>
2026-05-27 22:20:57 +0530

    updated some more bug for backend

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["./src/index.ts"],
   noExternal: [/@repo/], // transpile all @repo workspace packages and their dependencies
+  format: ["esm"],
   splitting: false,
   bundle: true,
   outDir: "./dist",
